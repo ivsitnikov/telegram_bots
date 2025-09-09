3a1cac5e28781253700f4bb79c2ae145d6dd94c5
Иван <sitnikov_ivan@outlook.com>
2025-09-09 18:49:36 +0300

    add corutine info, add_to_list, show_list

--- first_bot/bot.py
+++ first_bot/bot.py
@@ -2,8 +2,11 @@ import os
 import dotenv
 import asyncio
 import logging
+from datetime import datetime
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
+from aiogram.enums.dice_emoji import DiceEmoji
+
 
 # Включаем логгирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
@@ -17,15 +20,38 @@ bot = Bot(token=token)
 dp = Dispatcher()
 
 
-@dp.message()
+@dp.message(Command("dice"))
+async def cmd_dice(message: types.Message, bot: Bot):
+    await bot.send_dice(1601602567, emoji=DiceEmoji.DICE)
+
+
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message) -> None:
     await message.answer("Hello!")
 
 
+@dp.message(Command("add_to_list"))
+async def cmd_add_to_list(message: types.Message, my_list: list[int]):
+    my_list.append(7)
+    await message.answer("Добавлено число 7")
+
+
+@dp.message(Command("show_list"))
+async def cmd_show_list(message: types.Message, my_list: list[int]):
+    await message.answer(f"Ваш список {my_list}")
+
+
+@dp.message(Command("info"))
+async def cmd_info(message: types.Message, started_at: str):
+    await message.answer(f"Бот запущен {started_at}")
+
+
 # Запуск процесса полинга новых апдейтов
 async def main():
-    await dp.start_polling(bot)
+    # Иногда при старте бота требуется передать одно или несколько
+    # дополнительных значений
+    dp["started_at"] = datetime.now().strftime("%Y-%m-%d %H:%M")
+    await dp.start_polling(bot, my_list=[1, 2, 3])
 
 
 if __name__ == "__main__":
