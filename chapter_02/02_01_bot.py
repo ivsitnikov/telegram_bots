dc6f50bbc6ae4c165fe4da7044248ce66455c7b2
sitnikov ivan <sitnikov_ivan@outlook.com>
2025-09-16 06:50:57 +0300

    add function any_message

--- chapter_02/02_01_bot.py
+++ chapter_02/02_01_bot.py
@@ -14,3 +14,13 @@ logging.basicConfig(level=logging.INFO)
 dp = Dispatcher()
 logger.info(config.bot_token)
 bot = Bot(token=config.bot_token.get_secret_value())
+
+dp.message(F.text, Command('text'))
+async def any_message(message: Message): 
+    await message.answer(
+            "Hello, <b>world</b>!",
+            parse_mode=ParseMode.HTML
+            ) 
+    await message.answer("Hello, *world*\!",
+                         parse_mode=ParseMode.MARKDOWND_V2
+                         )
