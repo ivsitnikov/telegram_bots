d5eb636ced9ddf5d461893ac4786ae95ed9290d9
sitnikov ivan <sitnikov_ivan@outlook.com>
2025-11-10 16:54:22 +0300

    create handlers

--- book_bot/handlers/users.py
+++ book_bot/handlers/users.py
@@ -2,11 +2,13 @@ from copy import deepcopy
 from aiogram import Router, F
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message, CallbackQuery
+from book_bot.keyboards import pagination_kb
 from keyboards import (
     create_edit_keyboard,
     create_bookmarks_keyboard,
     create_pagination_kb,
 )
+from filters import IsDigitCallbackData, IsDelBookmarkCallbackData
 from lexicon import LEXICON_RU
 from database import db_init
 
@@ -30,38 +32,104 @@ async def process_command_help(message: Message):
 
 
 # Этот хэндлер будет срабатывать на команду /begining
-@user_router.message(Command(commands='beginning')):
-    async def process_command_begining(message: Message, book: dict, db: dict):
-        db['users'][message.from_user.id]['page'] = 1
-        text = book[1]
-        await message.answer(text=text,
-                             reply_markup=create_pagination_kb('backward',
-                                                               f'1/{len(book)}',
-                                                               'forward',
-                                                               ),
-                             )
+@user_router.message(Command(commands="beginning"))
+async def process_command_begining(message: Message, book: dict, db: dict):
+    db["users"][message.from_user.id]["page"] = 1
+    text = book[1]
+    await message.answer(
+        text=text,
+        reply_markup=create_pagination_kb(
+            "backward",
+            f"1/{len(book)}",
+            "forward",
+        ),
+    )
 
 
 # Этот хэндлер будет срабатывать на команду /continue
-@user_router.message(Command(commands='continue'))
+@user_router.message(Command(commands="continue"))
 async def process_command_continue(message: Message, book: dict, db: dict):
-    text = book[db[message.from_user.id]['page']]
-    await message.answer(text=text,
-                         reply_markup=create_pagination_kb('backward',
-                                                           f'{db['users'][message.from_user.id]['page']}/{len(book)}',
-                                                           'forward',
-                                                           ),
-                         )
+    text = book[db[message.from_user.id]["page"]]
+    await message.answer(
+        text=text,
+        reply_markup=create_pagination_kb(
+            "backward",
+            f"{db['users'][message.from_user.id]['page']}/{len(book)}",
+            "forward",
+        ),
+    )
 
 
 # Этот хэндлер будет срабатывать на команду /bookmarks
-@user_router.message(Command(commands='bookmarks'))
+@user_router.message(Command(commands="bookmarks"))
 async def process_command_continue(message: Message, book: dict, db: dict):
-    if db['users'][message.from_user.id]['bookmarks']:
-        await message.answer(text=LEXICON_RU[message.text], 
-                             reply_markup=create_bookmarks_keyboard(*db['users'][message.from_user.id]['bookmarks'], 
-                                                                    book=book,
-                                                                    )
-                             )
+    if db["users"][message.from_user.id]["bookmarks"]:
+        await message.answer(
+            text=LEXICON_RU[message.text],
+            reply_markup=create_bookmarks_keyboard(
+                *db["users"][message.from_user.id]["bookmarks"],
+                book=book,
+            ),
+        )
     else:
-        await message.answer(text=LEXICON_RU['no_bookmarks'],)
+        await message.answer(
+            text=LEXICON_RU["no_bookmarks"],
+        )
+
+
+# Этот хэндлер будет срабатывать на нажатие кнопки "вперед"
+@user_router.callback_query(F.data == "forward")
+async def process_forward_press(callback: CallbackQuery, book: dict, db: dict):
+    current_page = db["users"][callback.from_users.id]["page"]
+    if current_page < len(book):
+        db["users"][callback.from_user.id]["page"] += 1
+        text = book[current_page + 1]
+        await callback.message.edit_text(
+            text=text,
+            reply_markup=create_pagination_kb(
+                "backward",
+                f"{current_page}/len(book)",
+                "forward",
+            ),
+        )
+    await callback.answer()
+
+
+# Этот хэндлер будет срабатывать при нажатии на кнопку "назад"
+@user_router.callback_query(F.data == "backward")
+async def process_backward_press(callback: CallbackQuery, book: dict, db: dict):
+    current_page = db["users"][callback.from_user.id]["page"]
+    if current_page > 1:
+        db["users"][callback.from_user.id]["page"] -= 1
+        text = book[current_page - 1]
+        await callback.message.edit_text(
+            text=text,
+            reply_markup=create_pagination_kb(
+                "backward",
+                f"{db['users'][callback.from_user.id]['page']}/{len(book)}",
+                "forward",
+            ),
+        )
+    else:
+        await callback.answer()
+
+
+# Этот хэндлер будет срабатывать при нажатии на кнопку из списка закладок
+@user_router.callback_query(IsDigitCallbackData)
+async def process_bookmark_press(callback: CallbackQuery, book: dict, db: dict):
+    text = book[int(callback.data)]
+    db["users"][callback.from_user.id]["page"] = int(callback.data)
+    await callback.message.edit_text(
+        text=text,
+        reply_markup=create_pagination_kb(
+            "backward",
+            f"{db['users'][callback.from_user.id]['page']}/{len(book)}",
+            "forward",
+        ),
+    )
+
+
+# Этот хэндлер будет срабатывать при нажатии на инлайн кнопку "редактировать"
+# под списком закладок
+@user_router.callback_query(IsDelBookmarkCallbackData)
+async def process_edit_press(callback: CallbackQuery, book: dict, db: dict): ...
